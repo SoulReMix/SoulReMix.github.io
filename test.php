29c4807fc7d47814387a60e3bbee5b38af992de3
SoulReMix <tutiil5555@gmail.com>
2024-05-15 14:04:35 +0300

    First commit

---
+++ test.php
@@ -0,0 +1,8 @@
+<?
+    $frst=$_GET['fquestion'];
+    $scnd=$_GET['squestion'];
+    $thrd=$_GET['tquestion'];
+    if($frst!= NULL){
+        echo"Form working correctly and sending data to PHP";
+    }
+?>
